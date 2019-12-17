81f3b80209b7931ebd53d748778a0bc9f01858c5
Aaron Brodeur <aaronb@gmail.com>
2019-12-16 18:06:24 -0800

    Resources resources resources!

--- layout_structures/resources.js
+++ layout_structures/resources.js
@@ -10,36 +10,33 @@ const resources = () => (
     <Panel>
       <div>
         <Headline
-          title=""
-          subtitle=""
+          title="Books, sources of information and teachers that have helped shape our understanding of the world"
+          subtitle="LIBRARY OF RESOURCES"
         />
-        <p></p>
-        <Button to="/"></Button>
+        <Button to="#peers_and_allies">Peers & allies</Button>
+        <Button to="#books">Books</Button>
+        <Button to="#articles">Articles</Button>
+        <Button to="#podcasts">Podcasts</Button>
+        <Button to="#authors_and_artists">Authors & artists</Button>
       </div>
     </Panel>
 
     <Panel>
-      <div>
-        <Image filename="" alt="" />
-        <span></span>
-      </div>
       <div>
         <Headline
-          title=""
-          subtitle=""
+          title="Peers & Allies"
         />
-        <p></p>
-        <Button to="/"></Button>
+        <p>Communities of practice we see working on similar issues</p>
       </div>
-    </Panel>
-
-    <Panel>
       <div>
         <ul>
           <li>
-            <h2></h2>
+            <h2>Tamera</h2>
             <div>
-              <p></p>
+              <p>Tamera is a peace research village with the goal of becoming "a self-sufficient, sustainable and duplicable communitarian model for nonviolent cooperation and cohabitation between humans, animals, nature, and Creation for a future of peace for all." It is also often called a "healing biotope".</p>
+            </div>
+            <div>
+              <Link to="#">Learn More</Link>
             </div>
           </li>
           <li>
