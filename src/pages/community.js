c08c1a29ad78154e34886d69a56cb1c10f5f1ff8
Aaron Brodeur <aaronb@gmail.com>
2019-12-17 17:10:18 -0800

    Community first pass

--- src/pages/community.js
+++ src/pages/community.js
@@ -24,12 +24,12 @@ const community = () => (
       </div>
     </Panel>
 
-    <Panel>
-      <div>
+    <Panel justification="center">
+      <div class="twoColumnsLeftImage">
         <Image filename="community/community_charadrius_vociferus2x.jpg" alt="Charadrius vociferus" />
         <span>Charadrius vociferus</span>
       </div>
-      <div>
+      <div class="twoColumnsRightText">
         <Headline
           title="Join the conversation"
         />
@@ -49,7 +49,7 @@ const community = () => (
     </Panel>
 
     <Panel>
-      <div>
+      <div className="gatherings">
         <ul>
           <li>
             <h2>Gather in person</h2>
@@ -80,119 +80,217 @@ const community = () => (
       </div>
     </Panel>
     <Panel>
+      <div className="singleColumnImage">
         <Image filename="community/community_canis_latrans2x.jpg" alt="Canis latrans" />
         <span>Canis latrans</span>
+      </div>
     </Panel>
     <Panel>
-      <div>
+      <div className="singleColumnText">
         <Headline
           title="ORGANIZATIONS WE TRUST"
           subtitle="Bioregional Allies"
         />
         <p>The following is an incomplete list of organizations we see working on, experimenting with or practicing regeneration in the San Francisco Bay Area bioregion. If you are engaged in this work and not on this list, we would love to connect and understand how to amplify your work.</p>
 
-        <ul>
+        <ul className="smallWhiteCards">
           <li>
-            <h3>NorCal Resilience Network</h3>
-            <span></span>
+            <div>
+              <a href="https://www.norcalresilience.org/">
+                <h3>NorCal Resilience Network</h3>
+                <span>The mission of the NorCal Resilience Network is to catalyze a Just Transition to an equitable and regenerative region by supporting and activating community-based and ecological solutions in Northern California.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Permaculture Action Network</h3>
-            <span></span>
+            <div>
+              <a href="https://www.permacultureaction.org/">
+                <h3>Permaculture Action Network</h3>
+                <span>Permaculture Action Network is an organization that mobilizes concert-goers and festival-attendees to come out to "Permaculture Action Days," one day events where participants take direct action to build permaculture systems in the cities where they live.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Urban Permaculture Institute</h3>
-            <span></span>
+            <div>
+              <a href="https://www.upisf.com/">
+                <h3>Urban Permaculture Institute</h3>
+                <span>Helping make permaculture design relevant and accessible to people living in cities for individual and community resilience.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>BFI</h3>
-            <span></span>
+            <div>
+              <a href="http://www.bfi.org">
+                <h3>BFI</h3>
+                <span>The Buckminster Fuller Institute (BFI) is dedicated to catalyzing transformative solutions to complex global problems through design thinking education.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Bioneers</h3>
-            <span></span>
+            <div>
+              <a href="http://www.bioneers.org">
+                <h3>Bioneers</h3>
+                <span>Changing the world by changing the story with award-winning media on visionary and practical solutions for people and planet.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Black Mountain Circle</h3>
-            <span></span>
+            <div>
+              <a href="https://blackmountaincircle.org/">
+                <h3>Black Mountain Circle</h3>
+                <span>Black Mountain Circle cultivates resilience, community, and earth stewardship in perilous times. Through inspiring events and insightful conversations, we reclaim our personal and collective stories about nature and spirit.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Center for Evolutionary Leadership</h3>
-            <span></span>
+            <div>
+              <a href="https://evolutionleader.com/">
+                <h3>Center for Evolutionary Leadership</h3>
+                <span></span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Center for Humane Tech</h3>
-            <span></span>
+            <div>
+              <a href="https://humanetech.com/">
+                <h3>Center for Humane Tech</h3>
+                <span>Envisions a world where technology supports our shared well-being, sense-making, democracy, and ability to tackle complex global challenges.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Civana</h3>
-            <span></span>
+            <div>
+              <a href="https://civana.org/">
+                <h3>Civana</h3>
+                <span>A global community committed to working together on humanity's greatest challenges.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Comakery</h3>
-            <span></span>
+            <div>
+              <a href="https://info.comakery.com/">
+                <h3>Comakery</h3>
+                <span>We envision a future of distributed individuals pooling their talents to accelerate development of digital projects, while being fairly paid for their efforts. This is the foundation CoMakery is built from.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Burners Without Borders</h3>
-            <span></span>
+            <div>
+              <a href="https://www.burnerswithoutborders.org/mission">
+                <h3>Burners Without Borders</h3>
+                <span>Burners Without Borders (BWB) is a grassroots, volunteer-driven, community leadership organization whose goal is to unlock the creativity of local communities to solve problems that bring about meaningful change.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Gill Tract Farm</h3>
-            <span></span>
+            <div>
+              <a href="https://www.gilltractfarm.org/">
+                <h3>Gill Tract Farm</h3>
+                <span>Collaborative community-driven research, education, and extension focused on ecological farming and food justice. UC Gill Tract Community Farm.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Haight Street Commons</h3>
-            <span></span>
+            <div>
+              <a href="http://haight-st-commons.org/">
+                <h3>Haight Street Commons</h3>
+                <span>DIY Cooperative Housing in San Francisco, CA.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>LIFT economy</h3>
-            <span></span>
+            <div>
+              <a href="https://www.lifteconomy.com/">
+                <h3>LIFT economy</h3>
+                <span>LIFT Economy is an impact consulting firm, worker-owned cooperative, and Certified B Corporation.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Pachamama Alliance</h3>
-            <span></span>
+            <div>
+              <a href="https://www.pachamama.org/">
+                <h3>Pachamama Alliance</h3>
+                <span>The Pachamama Alliance is a not-for-profit organization that was born out of a relationship between a group of people from the modern world and the leaders of remote indigenous groups in the Amazon region of Ecuador.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Regenesis Institute</h3>
-            <span></span>
+            <div>
+              <a href="https://regenerat.es/">
+                <h3>Regenesis Institute</h3>
+                <span>The Regenesis Institute was formed in 2018 as a nonprofit organization dedicated to providing education pathways for regenerative practitioners around the world.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>San Francisco Permaculture Guild</h3>
-            <span></span>
+            <div>
+              <a href="https://www.permaculture-sf.org/">
+                <h3>San Francisco Permaculture Guild</h3>
+                <span>To support a local community of those interested in permaculture by producing educational and social events, enabling permaculture projects and providing opportunities to exchange information related to permaculture design.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Sogora Te Land Trust</h3>
-            <span></span>
+            <div>
+              <a href="https://sogoreate-landtrust.com/">
+                <h3>Sogora Te Land Trust</h3>
+                <span>The Sogorea Te Land Trust is an urban Indigenous women-led community organization that facilitates the return of Chochenyo and Karkin Ohlone lands in the San Francisco Bay Area to Indigenous stewardship.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>The Center SF</h3>
-            <span></span>
+            <div>
+              <a href="https://www.thecentersf.com/">
+                <h3>The Center SF</h3>
+                <span>Dedicated to the collective consciousness, serving to give back to the community.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Thrive East Bay</h3>
-            <span></span>
+            <div>
+              <a href="http://www.thriveeastbay.org/">
+                <h3>Thrive East Bay</h3>
+                <span>Thrive East Bay is growing meaningful community and creating a just and flourishing world for all.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Uptima Business Bootcamp</h3>
-            <span></span>
+            <div>
+              <a href="https://uptimabootcamp.com/">
+                <h3>Uptima Business Bootcamp</h3>
+                <span>Uptima Business Bootcamp is an innovative member-owned business accelerator dedicated to providing entrepreneurs with greater access to hands-on education, resources and community to create thriving businesses.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Weaving Earth</h3>
-            <span></span>
+            <div>
+              <a href="https://weavingearth.org/">
+                <h3>Weaving Earth</h3>
+                <span>Weaving Earth Center for Relational Education provides nature-based education for action at the confluence of ecological, social and personal systems change.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Whole Person Economy</h3>
-            <span></span>
+            <div>
+              <a href="https://wholepersoneconomy.com/">
+                <h3>Whole Person Economy</h3>
+                <span>WPE co-creates coherent and actionable alternatives to outdated economic ideas, works to prototype and scale new approaches, and drives resources to individuals and organizations that integrate holistic economics, finance, and business practices.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Wilderness Torah</h3>
-            <span></span>
+            <div>
+              <a href="https://wildernesstorah.org/">
+                <h3>Wilderness Torah</h3>
+                <span>Wilderness Torah awakens and celebrates the earth-based traditions of Judaism to nourish the connections between self, community, earth, and Spirit.</span>
+              </a>
+            </div>
           </li>
         </ul>
       </div>
     </Panel>
     <Panel>
-      <div>
+      <div className="singleColumnText">
         <Image filename="community/community_hygrocybe_coccinea2x.jpg" alt="Hygrocybe coccinea" />
         <span>Hygrocybe coccinea</span>
         <Headline
@@ -201,54 +299,102 @@ const community = () => (
         />
         <p>Here are a number of communities we know about practicing regeneration in big ways and small. If you have a land-based community practicing regeneration in the Bay Area, reach out to us: We want to meet you and support your work.</p>
 
-        <ul>
+        <ul className="smallWhiteCards">
           <li>
-            <h3>The Mushroom Farm</h3>
-            <span>Just south of Pescadero a group of people are creating the future of farming</span>
+            <div>
+              <a href="https://www.themushroom.farm/">
+                <h3>The Mushroom Farm</h3>
+                <span>The Mushroom Farm is a ~750 acre dream regeneration project on the California coastline.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Agapolis</h3>
-            <span>A community in the south bay supporting the future of coliving</span>
+            <div>
+              <a href="https://www.agapolis.org/">
+                <h3>Agapolis</h3>
+                <span>Situated on 22 acres of land in Portola Valley, California, Agapolis is a home, community, and gathering place for people contributing to the evolution of consciousness, culture, and systems. </span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Canticle Farm</h3>
-            <span>A Franciscan community tearing down fences and practicing radical community and urban agriculture</span>
+            <div>
+              <a href="https://www.canticlefarm.org/">
+                <h3>Canticle Farm</h3>
+                <span>Canticle Farm is an urban farm and educational center, an intentional community, and an experiment in faith-based activism, nonviolence, and restorative justice.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Commonweal</h3>
-            <span></span>
+            <div>
+              <a href="https://www.commonweal.org/">
+                <h3>Commonweal</h3>
+                <span>Today, Commonweal works in three core fields—health and healing, art and education, and environment and justice. </span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Cosmic Honey</h3>
-            <span></span>
+            <div>
+              <a href="">
+                <h3>Cosmic Honey</h3>
+                <span></span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Embassy</h3>
-            <span></span>
+            <div>
+              <a href="https://embassynetwork.com/">
+                <h3>Embassy</h3>
+                <span>Together, we start and support spaces that prototype new approaches to an open, creative society. These spaces are embassies of a future that is abundant, collaborative and transparent.</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Heartland</h3>
-            <span></span>
+            <div>
+              <a>
+                <h3>Heartland</h3>
+                <span></span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Landwell</h3>
-            <span></span>
+            <div>
+              <a>
+                <h3>Landwell</h3>
+                <span></span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>PLACE for sustainable living</h3>
-            <span></span>
+            <div>
+              <a href="https://www.place.community/">
+                <h3>PLACE for sustainable living</h3>
+                <span>A volunteer-run community hub that fosters regenerative living practices through People Linking Art, Community & Ecology</span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Rivers Bend Retreat Center</h3>
-            <span></span>
+            <div>
+              <a href="http://www.riversbendretreat.org">
+                <h3>Rivers Bend Retreat Center</h3>
+                <span>River's Bend Retreat Center founded first as a family retreat now continues the tradition of warm hospitality for those wanting to experience country living in an exceptional natural setting. </span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Aletheia</h3>
-            <span></span>
+            <div>
+              <a href="https://www.aletheiasprings.com/">
+                <h3>Aletheia</h3>
+                <span>We are Aletheia Springs, a multigenerational community stewarding and working to regenerate the land and waters of a historic 20-acre geothermal mineral springs in the heart of Sonoma Valley, Northern California </span>
+              </a>
+            </div>
           </li>
           <li>
-            <h3>Wyldflower</h3>
-            <span></span>
+            <div>
+              <a>
+                <h3>Wyldflower</h3>
+                <span></span>
+              </a>
+            </div>
           </li>
         </ul>
       </div>
