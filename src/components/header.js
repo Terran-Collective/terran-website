a862718b8a5dee76e7140d831bc7e6503bc20ec1
Tibet Sprague <tibet.sprague@gmail.com>
2019-12-21 15:28:13 -0800

    Get opening/closing of menu to work

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,54 @@
+import classnames from "classnames";
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import logo from "../images/logos/terran_collective_logo_blue.svg"
 import Menu from "./menu"
 import menuToggleHamburger from "../images/icon/menu_hamburger.svg"
 import menuToggleX from "../images/icon/menu_x.svg"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div
-      style={{
-        display: `flex`,
-        justifyContent: `space-between`,
-        alignItems: `center`,
-        margin: `0 auto`,
-        padding: `20px`,
-        position: `absolute`,
-        width: `100%`,
-        zIndex: `2`,
-      }}
-    >
-      <h1 style={{ margin: 0, height: "35px" }}>
-        <Link
-          to="/"
-          style={{
-            color: `#2b80f0`,
-            textDecoration: `none`,
-          }}
-        >
-          <img src={logo} alt="Terran Collective" />
-        </Link>
-      </h1>
-      <div class="menuWrapper">
-        <button class="menuToggle">
-            <img src={menuToggleHamburger} alt="Open menu icon" class="openMenu" />
-            <img src={menuToggleX} alt="Close menu icon"class="closeMenu" />
-        </button>
-        <div class="menuBg"></div>
-        <div class="menuContainer">
-          <Menu/>
+const Header = ({ siteTitle }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <header>
+      <div
+        style={{
+          display: `flex`,
+          justifyContent: `space-between`,
+          alignItems: `center`,
+          margin: `0 auto`,
+          padding: `20px`,
+          position: `absolute`,
+          width: `100%`,
+          zIndex: `2`,
+        }}
+      >
+        <h1 style={{ margin: 0, height: "35px" }}>
+          <Link
+            to="/"
+            style={{
+              color: `#2b80f0`,
+              textDecoration: `none`,
+            }}
+          >
+            <img src={logo} alt="Terran Collective" />
+          </Link>
+        </h1>
+        <div class={classnames({ "menuWrapper" : true, "open" : isOpen })}>
+          <button class="menuToggle" onClick={() => setOpen(!isOpen)}>
+              <img src={menuToggleHamburger} alt="Open menu icon" class="openMenu" />
+              <img src={menuToggleX} alt="Close menu icon"class="closeMenu" />
+          </button>
+          <div class="menuBg"></div>
+          <div class="menuContainer">
+            <Menu/>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
