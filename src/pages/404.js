6df08d641d54392aae81d05e65b0d984325e82ee
Tibet Sprague <tibet.sprague@gmail.com>
2019-12-22 14:36:33 -0800

    Slightly improve 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,11 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <div style={{ position: "relative", top: "86px", left: "27px", height: "225px" }}>
+      <h1>Page not found</h1>
+      <p>You seem to be lost! Check out all the cool pages in the footer below</p>
+    </div>
   </Layout>
 )
 
