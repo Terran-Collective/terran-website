098538188734cfceb94f87fa8e9efcc0b8d17e0b
Tibet Sprague <tibet.sprague@gmail.com>
2019-12-22 14:36:15 -0800

    Fix up site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Terran`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Terran Collective`,
+    description: `Amplifying cooperation in service to regeneration in the Bay Area bioregion through technology, collective action and rebuilding the commons`,
+    author: `@TerranCollectiv`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
