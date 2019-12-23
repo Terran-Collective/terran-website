759adac54a80a555b64e3dd46496d757eff9549f
Aaron Brodeur <aaronb@gmail.com>
2019-12-22 17:33:38 -0800

    Watershed

--- src/pages/watershed.js
+++ src/pages/watershed.js
@@ -44,9 +44,9 @@ const watershed = () => (
           title="We need your help to fill the watershed"
         />
         <p>We have structured our organization in a way that represents our values, creating a diversity of options for how we can fill our wells, and to allow supporters and partners to contribute resources to our mission. Currently we manage a for-profit entity called the Terran Collective LLC, which mostly does mostly contract software development work with aligned partners like DAOstack as our primary source of income. Moving forward we want to move our work more and more into the gift, and this is where the Terran Watershed comes in as the source of life that will allow us to more fully pursue our purpose and share our gifts with the world. </p>
-        <p>Terran Watershed is a non-profit 501c3, fiscally sponsored by our friends and allies at Empowerment Works. We are asking for contributions to our Watershed in form of tax deductible donations.  </p>
+        <p>Terran Watershed is a non-profit 501c3, fiscally sponsored by our friends and allies at <a href="https://www.empowermentworks.org/">Empowerment Works</a>. We are asking for contributions to our Watershed in form of tax deductible donations.  </p>
         <p>Over the past three years we have built a software platform for matching needs and offers, hosted many community events, mapped important relationship and energy landscapes within our bioregion and researched and developed systems for distributed collective governance and ownership. We have been able to do this without funding, and are looking to evolve into the next level of our stewardship of this work.</p>
-        <p>Our work of software development, community building, supporting fellow players in regeneration work, organizing action days, and sharing tools is in the form of a gift. In order to bestow this gift, your donations will provide resources for the Core team to thrive and do their life’s work. Your donations into The Watershed allow for a continuation of our research, development, education, community building, service and tools. </p>
+        <p>Our work of software development, community building, supporting fellow players in regeneration work, organizing action days, and sharing tools is in the form of a gift. In order to bestow this gift, your donations will provide resources for the Core team to thrive and do their life’s work. Your donations into The Watershed allow for a continuation of our research, development, education, community building, service and tools.</p>
       </div>
     </Panel>
 
@@ -60,7 +60,7 @@ const watershed = () => (
             <img src={resourceUse1} alt="Mapping & Network building"/>
             <h2>Mapping &amp; Network building</h2>
             <div>
-              <p>In order to transition from an extractive system to a closed-loop, regenerative system, our landscape needs to be mapped and new networks must be built. The rise of ridesharing and homesharing marketplace technologies are a hint of what is possible if we are able to map needs, offers, abilities, untapped resources and knowledge. </p>
+              <p>In order to transition from an extractive system to a closed-loop, regenerative system, our landscape needs to be mapped and new networks must be built. The rise of ridesharing and homesharing marketplace technologies are a hint of what is possible if we are able to map needs, offers, abilities, untapped resources and knowledge.</p>
               <p>During 2020 we will continue to map the landscape of regenerative projects in the San Francisco Bay Area. With this community as our first audience, our research seeks to uncover ways of surfacing the hidden landscape of the human and natural world, and build networks so that resources, energy and knowledge can flow freely outside of the market.</p>
             </div>
           </li>
@@ -117,10 +117,10 @@ const watershed = () => (
           />
           <p>We practice full transparency in finances and invite you to take a look at a draft of our operating budget.</p>
           <div className="centerText">
-          <Button link="/budget" label="Our 2020 Budget Goals" />
+            <a class="button" href="https://docs.google.com/spreadsheets/d/1caGcyCFm1hwtIH-ER3saREb1lKMffNTAwcdm2g7lCls/edit#gid=0">Our 2020 Budget Goals</a>
           </div>
           <p>For the last three years we have contributed our extra time and energy, and have supported each other financially to work on this vision. We are committed to building a way of life that realizes the hopes in our hearts, and we see this as a lifetime project. Any resources we receive, any help we receive, will help us to accomplish our goals sooner, and be able to share our research, tools and systems with the world more rapidly</p>
-          <p>We are doing this work for the people and the Earth, not to enrich the wealthy. We don’t expect traditional investment structures such as venture capital to understand the value of this work, so we are asking you, our community, to join with us in creating a better world.Please consider giving a monthly donation of time or money. We believe that the community patronage model could be an example of how we can fund the transformative work that is required at this time. We are also of course happy to accept larger one time donations  to give us a big rocket boost as we leap into this work whole heartedly. </p>
+          <p>We are doing this work for the people and the Earth, not to enrich ourselves or investors. We don’t expect traditional investment structures such as venture capital to understand the value of this work, so we are asking you, our community, to join with us in creating a better world. Please consider giving a monthly donation of time or money. We believe that the community patronage model could be an example of how we can fund the transformative work that is required at this time. We are also of course happy to accept larger one time donations  to give us a big rocket boost as we leap into this work whole-heartedly.</p>
           <p>We are asking you to trust us and join us in this radical experiment, to see if we can give ourselves over completely to be of service to our bioregion and the world. Now is the time for to put our energy and resources into radical experimentation and change. The world is calling for a transition from the extractive economy into the regenerative economy, so we can liberate our communities, ecosystems, and align with our true value systems. And stop us from destroying ourselves and the planet. Help us begin right now, right here, with us.</p>
           <div className="centerText">
             <Button link="#donate" label="Become a divestor" />
